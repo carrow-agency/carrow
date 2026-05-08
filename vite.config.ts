a94bf5e4b58f0757feca31c89684e3a6efabfbb4
Kenz Bilal <kenzbilal@gmail.com>
2026-05-08 16:06:23 +0530

    Enable Sentry source maps for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
@@ -12,18 +12,18 @@ export default defineConfig(({mode}) => {
   }
 
   return {
-    plugins: [react(), tailwindcss()],
-    // plugins: [
-    //   react(),
-    //   tailwindcss(),
-    //   sentryVitePlugin({
-    //     org: "kenz-bilal",
-    //     authToken: process.env.SENTRY_AUTH_TOKEN,
-    //     sourcemaps: {
-    //       filesToDeleteAfterUpload: ["./**/*.map"],
-    //     },
-    //   }),
-    // ],
+    plugins: [
+      react(),
+      tailwindcss(),
+      sentryVitePlugin({
+        org: "kenz-bilal",
+        project: "carrow",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        sourcemaps: {
+          filesToDeleteAfterUpload: ["./**/*.map"],
+        },
+      }),
+    ],
     define: {
       'import.meta.env.VITE_CONVEX_URL': JSON.stringify(env.VITE_CONVEX_URL),
     },
@@ -36,8 +36,9 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR === 'true' ? false : { overlay: true },
     },
     build: {
-      outDir: "dist",
-      sourcemap: false,
+      outDir: "dist-new",
+      emptyOutDir: true,
+      sourcemap: mode === 'development',
       minify: mode === 'production',
       rollupOptions: {
         output: {
