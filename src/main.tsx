bb4f740cd4e085fecf11ccc5e6aa61299986c449
Kenz Bilal <kenzbilal@gmail.com>
2026-05-08 16:14:09 +0530

    Fix Sentry config

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConvexProvider } from 'convex/react';
 import { ConvexReactClient } from 'convex/react';
